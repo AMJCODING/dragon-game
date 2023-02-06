eb0a772db6f73d8c7661d9ae836d098913553687
AMJCODING <amj.code.2007@gmail.com>
2023-02-06 11:31:28 +0300

    new block added

--- dragon.js
+++ dragon.js
@@ -2,6 +2,8 @@ let char = document.getElementById("char");
 
 let block = document.getElementById("block");
 
+let block1 = document.getElementById("block1");
+
 
 function jumping(){ 
 
@@ -15,6 +17,22 @@ function jumping(){
 
 }
 
+
+ function down_animation(){
+
+    char.classList.add("char_down")
+
+    setTimeout(() => {
+        char.classList.remove("char_down")
+    }, 1000);
+
+ }
+
+
+
+
+
+
 setInterval(() => {
     let char_top = parseInt(window.getComputedStyle(char).getPropertyValue("top"))
 
@@ -25,7 +43,7 @@ setInterval(() => {
     let block_height =  parseInt(window.getComputedStyle(block).getPropertyValue("height"))
 
 
-    if(block_left<char_width && block_left>0 && char_top>=600 - block_height ){
+    if(block_left<char_width-5 && block_left>0 && char_top>=600 - block_height ){
 
 let lose_msg =         confirm("you lose")
         block.style.animation = "none"
@@ -34,10 +52,33 @@ let lose_msg =         confirm("you lose")
         if(lose_msg ===true){
             window.open("http://127.0.0.1:5500/dragon.html")
         }
-
     }
 }, 10);
 
+
+
+
+setInterval(() => {
+    let char_top = parseInt(window.getComputedStyle(char).getPropertyValue("top"))
+
+    let char_width = parseInt(window.getComputedStyle(char).getPropertyValue("width"))
+
+    let block1_left = parseInt(window.getComputedStyle(block1).getPropertyValue("left"))
+
+    let block1_height =  parseInt(window.getComputedStyle(block1).getPropertyValue("height"))
+
+
+    if(block1_left<char_width-5 && block1_left>0 && char_top<=640 ){
+
+let lose_msg =         confirm("you lose")
+        block.style.animation = "none"
+        block.style.display = "none"
+
+        if(lose_msg ===true){
+            window.open("http://127.0.0.1:5500/dragon.html")
+        }
+    }
+}, 10);
 
 
 
