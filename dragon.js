818e2f1d704689d4abac86356bb4ed57a5888e91
AMJCODING <amj.code.2007@gmail.com>
2023-02-12 00:16:23 +0300

    Add "right  , "left" buttons

--- dragon.js
+++ dragon.js
@@ -5,6 +5,7 @@ let block = document.getElementById("block");
 let block1 = document.getElementById("block1");
 
 
+//jump function
 function jumping(){ 
 
 
@@ -13,15 +14,9 @@ function jumping(){
 
     setTimeout(() => {
         char.classList.remove("char_jump")
-    }, 800);
+    }, 1000);
 
 }
-
-
-
-
-
-
 
 setInterval(() => {
     let char_top = parseInt(window.getComputedStyle(char).getPropertyValue("top"))
@@ -32,25 +27,36 @@ setInterval(() => {
 
     let block_height =  parseInt(window.getComputedStyle(block).getPropertyValue("height"))
 
+    let char_left = parseInt(window.getComputedStyle(char).getPropertyValue("left"))
 
-    if(block_left<char_width-5 && block_left>0 && char_top>=600 - block_height ){
+
+    if(block_left == char_left + char_width && block_left > 0 && char_top >= 600 - block_height ){
+
 
         alert("you lose")
-        block.style.animation = "none"
-        block.style.display = "none"
+        block.style.animation = "none";
+        block.style.display = "none";
+        block1.style.animation = "none";
+        block1.style.display = "none";
 
     }
 }, 10);
 
 
 
+
+
+
+
+
+//down function
 function down_animation(){
 
     char.classList.add("char_down")
 
     setTimeout(() => {
         char.classList.remove("char_down")
-    }, 800);
+    }, 1000);
 
  }
 
@@ -62,14 +68,17 @@ setInterval(() => {
 
     let block1_left = parseInt(window.getComputedStyle(block1).getPropertyValue("left"))
 
-    let block1_height =  parseInt(window.getComputedStyle(block1).getPropertyValue("height"))
+    let char_left = parseInt(window.getComputedStyle(char).getPropertyValue("left"))
 
 
-    if(block1_left<char_width-5 && block1_left>0 && char_top<=640 ){
+    if(block1_left == char_left+char_width && block1_left >0 && char_top<=640 ){
 
         alert("you lose")
-        block.style.animation = "none"
-        block.style.display = "none"
+        block1.style.animation = "none";
+        block1.style.display = "none";
+        block.style.animation = "none";
+        block.style.display = "none";
+
 
     }
 }, 10);
@@ -77,11 +86,46 @@ setInterval(() => {
 
 
 
-counter
+
+//right function
+
+let counter = 0;
+
+
+
+function right_animation(){
+
+    if (counter !== 1600) {
+    
+        counter+=100;
+    
+    char.style.left = `${counter}`
+    
+    }
+    
+
+}
+
+
+function left_animation(){
+
+if (counter!==0) {
+    
+    counter-=100;
+
+char.style.left = `${counter}`
+
+}
+
+}
 
 
 
 
+
+
+
+//score counter
 let count_input = document.getElementById("counter");
 
 function countUp(){
